d90fda65aed59f1fc087a260e786324162b636c4
jignesh sharma <jigneshsharma9868@gmail.com>
2023-12-29 10:15:42 +0530

    implemented the example of the hierarchical inheritance

---
+++ inheritanceExmple/Hierarchical/A.java
@@ -0,0 +1,12 @@
+package inheritanceExmple.Hierarchical;
+
+public class A {
+    void sayA() {System.out.println("this is A");}
+}
+class B extends A {
+    void SayB() {System.out.println("this is the B");}
+}
+class C extends A {
+
+  void SayC() {System.out.println("this is the C");}
+}
---
+++ inheritanceExmple/Hierarchical/Main.java
@@ -0,0 +1,8 @@
+package inheritanceExmple.Hierarchical;
+
+public class Main {
+    public static void main(String[] args) {
+        new B().sayA();
+        new C().sayA();
+    }
+}
